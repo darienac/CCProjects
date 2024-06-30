43e7d1b628b97cb6bd05c08c9e4261a96494c63e
Darien Campbell <dacman9@gmail.com>
2024-06-30 13:15:44 -0400

    Added CCBuild

---
+++ CCBuild/ccbuild.lua
@@ -0,0 +1,28 @@
+--Constants
+local args = {...}
+
+--Functions
+local function loadFromPath(path, includedPaths)
+    local page = http.get(path)
+    local out = ""
+    for line in page.readLine() do
+        if string.sub(line, 1, 3) == "---" then
+            if string.sub(line, 1, 11) == "---include " then
+                line = string.sub(line, 12)
+                if not includedPaths[line] then
+                    includedPaths[line] = true
+                    out = out .. "\n" .. loadFromPath(line, includedPaths)
+                end
+            end
+        else
+            out = out .. "\n" .. line
+        end
+    end
+    page.close()
+    return out
+end
+
+--Main
+local f = fs.open(args[1], "w")
+f.write(loadFromPath(args[2], {}))
+f.close()
---
+++ Tortoise/api/api.lua
@@ -0,0 +1,27 @@
+local function Tortoise()
+    local NORTH = 0 -- -Z
+    local EAST = 1  -- +X
+    local SOUTH = 2 -- +Z
+    local WEST = 3  -- -X
+
+    local self = {
+        ["location"]={0,0,0},
+        ["direction"]=NORTH
+    }
+
+    function self:turnLeft()
+        -- if turtle.turnLeft() then
+            self.direction = (self.direction - 1) % 4
+        -- end
+    end
+
+    function self:turnRight()
+        -- if turtle.turnRight() then
+            self.direction = (self.direction + 1) % 4
+        -- end
+    end
+
+    return self
+end
+
+_G.t = Tortoise()
