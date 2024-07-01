ca0b3e2d9ef0284f176be9b3473b488591443920
Darien Campbell <dacman9@gmail.com>
2024-06-30 20:49:19 -0400

    Added intelligent control for managing fuel levels from a central home location

--- Tortoise/api/api.lua
+++ Tortoise/api/api.lua
@@ -3,11 +3,18 @@ local function Tortoise()
     local EAST = 1  -- +X
     local SOUTH = 2 -- +Z
     local WEST = 3  -- -X
+    local UP = 4
+    local DOWN = 5
     local FUEL_BUFFER = 1
 
     local self = {
         ["location"]={0,0,0},
         ["direction"]=NORTH,
+        ["homeLocation"]={0,0,0},
+        ["homeDirection"]=NORTH,
+        ["homeRefuelName"]="back",
+        ["homeStoreName"]="back",
+        ["refuelMax"]=90000,
         ["configPath"]=".tortoise"
     }
 
@@ -15,6 +22,11 @@ local function Tortoise()
         local f = fs.open(self.configPath, "w")
         f.writeLine("location " .. self.location[1] .. " " .. self.location[2] .. " " .. self.location[3])
         f.writeLine("direction " .. self.direction)
+        f.writeLine("homeLocation " .. self.homeLocation[1] .. " " .. self.homeLocation[2] .. " " .. self.homeLocation[3])
+        f.writeLine("homeDirection " .. self.homeDirection)
+        f.writeLine("homeRefuelName " .. self.homeRefuelName)
+        f.writeLine("homeStoreName " .. self.homeStoreName)
+        f.writeLine("refuelMax " .. self.refuelMax)
         f.close()
     end
 
@@ -32,6 +44,16 @@ local function Tortoise()
                 self.location = {tonumber(args[2]), tonumber(args[3]), tonumber(args[4])}
             elseif args[1] == "direction" then
                 self.direction = tonumber(args[2])
+            elseif args[1] == "homeLocation" then
+                self.homeLocation = {tonumber(args[2]), tonumber(args[3]), tonumber(args[4])}
+            elseif args[1] == "homeDirection" then
+                self.homeDirection = tonumber(args[2])
+            elseif args[1] == "homeRefuelName" then
+                self.homeRefuelName = args[2]
+            elseif args[1] == "homeStoreName" then
+                self.homeStoreName = args[2]
+            elseif args[1] == "refuelMax" then
+                self.refuelMax = tonumber(args[2])
             end
         end
         f.close()
@@ -125,6 +147,21 @@ local function Tortoise()
         self:save()
     end
 
+    function self:setHome(location, direction, refuel, store)
+        self.location = table.pack(table.unpack(location))
+        self.direction = direction
+        self.homeRefuelName = refuel
+        self.homeStoreName = store
+    end
+
+    function self:distanceBetween(loc1, loc2)
+        return math.abs(loc1[1] - loc2[1]) + math.abs(loc1[2] - loc2[2]) + math.abs(loc1[3] - loc2[3])
+    end
+
+    function self:distanceTo(location)
+        return self:distanceBetween(self.location, location)
+    end
+
     function self:faceTo(direction)
         if (self.direction - 1) % 4 == direction then
             return self:turnLeft()
@@ -173,6 +210,89 @@ local function Tortoise()
             zDone = location[3] == self.location[3]
         end
         return true
+    end
+
+    function self:goHome()
+        return self:moveTo(self.homeLocation) and self:faceTo(self.homeDirection)
+    end
+
+    function self:homeRefuel()
+        if not self:goHome() then
+            return false
+        end
+        if turtle.getItemCount(16) > 0 then
+            return false
+        end
+        turtle.select(16)
+        while turtle.getFuelLevel() < self.refuelMax and turtle.suckDown() do
+            turtle.refuel()
+        end
+        if turtle.getFuelLevel() >= self.refuelMax then
+            return true
+        end
+        local refuelStore = peripheral.wrap(self.homeRefuelName)
+        if not refuelStore then
+            return false
+        end
+        local helperStore = peripheral.wrap("bottom")
+        if not helperStore then
+            return false
+        end
+        while turtle.getFuelLevel() < self.refuelMax do
+            local itemCount = 0
+            for i = 1, refuelStore.size() do
+                itemCount = itemCount + helperStore.pullItems(self.homeRefuelName, i)
+            end
+            if itemCount == 0 then
+                break
+            end
+            while turtle.suckDown() do
+                turtle.refuel()
+            end
+        end
+        return true
+    end
+
+    function self:homeDeposit()
+        if not self:goHome() then
+            return false
+        end
+        local helperStore = peripheral.wrap("bottom")
+        if not helperStore then
+            return false
+        end
+        for i = 1,16 do
+            turtle.select(i)
+            if not turtle.dropDown() then
+                return false
+            end
+        end
+        local homeStore = peripheral.wrap(self.homeStoreName)
+        if not homeStore then
+            return true
+        end
+        for i = 1, helperStore.size() do
+            if not helperStore.pushItems(self.homeStoreName, i) then
+                return true
+            end
+        end
+        return true
+    end
+
+    function self:makeTripTo(location, allowDeposit)
+        if self:refuel(self:distanceTo(location) + self:distanceBetween(location, self.homeLocation) <= turtle.getFuelLevel()) then
+            return self:moveTo(location)
+        end
+        if not self:homeRefuel() then
+            return false
+        end
+        if allowDeposit then
+            self:homeDeposit()
+        end
+        if self:distanceTo(location) * 2 > turtle.getFuelLevel() then
+            return false
+        end
+        return self:moveTo(location)
     end
 
     self:load()
